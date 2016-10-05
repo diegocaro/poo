a5518241c90ebfe013c88ae48d4e208dfaddfce9
Diego Caro <diegocaro@gmail.com>
2016-10-05 11:32:56 -0300

    FLTK is working now

--- clases/poo-06-inheritance/cuadrado-herencia.cc
+++ clases/poo-06-inheritance/cuadrado-herencia.cc
@@ -7,6 +7,10 @@ class Rectangle {
     double x2_, y2_; //punto superior-derecho
     
 public:
+    Rectangle() {
+        x1_ = y1_ = -10; x2_ = y2_ = 10;
+        cout << "Constructor por defecto de Rectangle\n";
+    }
     Rectangle(double x1, double y1, \
                 double x2, double y2) {
         x1_ = x1; y1_ = y1;
@@ -21,13 +25,22 @@ public:
 
 class Cuadrado: public Rectangle {
 public:
-    Cuadrado(double x, double y, double side) : 
-        Rectangle(x, y, x+side, y+side) 
+    Cuadrado(double x, double y, double side)  
+      :  Rectangle(x, y, x+side, y+side) 
     {
         cout << "Constructor clase Cuadrado\n";
-    };
+    }
 };
 
+class TriRect: public Cuadrado {
+    public:
+        TriRect(double x, double y, double side)
+        : Cuadrado(x, y, side) {
+
+        cout << "Constructor Triangle\n";
+        }
+
+};
 
 int main() {
     Rectangle r(-1, -1, 1, 1);
@@ -36,5 +49,12 @@ int main() {
     Cuadrado c(-1, -1, 2);
     std::cout << "Area de c: " << c.area() << "\n";
     
+    cout << "Ancho cuadrado: " << c.width() << "\n";
+
+    TriRect t(-1, -1, 2);
+    double ar = t.area();
+    cout << "Area triangulo rectangulo: " << ar/2 << "\n";
+
+
     return 0;
 }
---
+++ tutorial_fltk/examples/build/CMakeFiles/input-output.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/input-output.dir/input-output.cc.o"
+  "input-output.pdb"
+  "input-output"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/input-output.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
